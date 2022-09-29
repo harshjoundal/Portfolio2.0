7612ec097a8ab7adc8e343a605056cdd78e0569d
harshjoundal <harshjoundal@gmail.com>
2022-09-29 22:01:31 +0530

    update

---
+++ src/components/Education/Data.jsx
@@ -0,0 +1,58 @@
+const Data = [
+    {
+      id: 1,
+      category: "education",
+      icon: "icon-graduation",
+      year: "2022 - present",
+        title: "MERN Stack Development",
+      college : 'Masai School',
+     
+    },
+    {
+      id: 2,
+      category: "education",
+      icon: "icon-graduation",
+      year: "2020 - 2021",
+    title: "Post Graduate Diploma (Data Science)",
+      college : "Amity University, Haryana",
+      desc: "8.9 CGPA",
+    },
+    {
+      id: 3,
+      category: "education",
+      icon: "icon-graduation",
+      year: "2017 - 2020",
+        title: "Bachelor's Degree (Mechanical Engineering)",
+        college : "Dr.D.Y.Patil College of Engineering",
+      desc: "7.1 CGPA",
+    }
+];
+  
+export const ExperianceData = [
+  {
+    id: 4,
+    category: "experience",
+    icon: "icon-briefcase",
+    year: "2019 - present",
+    title: "Web Designer",
+    desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
+  },
+  {
+    id: 5,
+    category: "experience",
+    icon: "icon-briefcase",
+    year: "2013 - 2017",
+    title: "Front-End Developer",
+    desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
+  },
+  {
+    id: 6,
+    category: "experience",
+    icon: "icon-briefcase",
+    year: "2009 - 2013",
+    title: "Back-End Developer",
+    desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
+  },
+]
+
+export default Data
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import leaf from "../../Assets/Projects/leaf.png";
+import emotion from "../../Assets/Projects/emotion.png";
+import editor from "../../Assets/Projects/codeEditor.png";
+import chatify from "../../Assets/Projects/chatify.png";
+import suicide from "../../Assets/Projects/suicide.png";
+import bitsOfCode from "../../Assets/Projects/blog.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={"https://user-images.githubusercontent.com/72031522/192807635-d833cb79-41d8-419f-9898-6ac98eec5b94.JPG"}
+              isBlog={false}
+              title="LYST Colne"
+              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              ghLink="https://github.com/harshjoundal/LYST"
+              demoLink="https://lystlyst.netlify.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={"https://user-images.githubusercontent.com/72031522/192809246-830bddb2-9ef5-4e14-be3b-6df7a3f1afb1.JPG"}
+              isBlog={false}
+              title="Tracking Time website clone"
+              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
+              ghLink="https://github.com/harshjoundal/TimeTracker"
+              demoLink="https://time-tracking-clone.netlify.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={"https://user-images.githubusercontent.com/72031522/192809888-5daf0490-3904-4c11-ad20-a9edf4dd4592.JPG"}
+              isBlog={false}
+              title="Full stack BlogApp"
+              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
+              ghLink="#"
+              demoLink="#"              
+            />
+          </Col>
+
+          {/* <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={leaf}
+              isBlog={false}
+              title="Plant AI"
+              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
+              ghLink="https://github.com/soumyajit4419/Plant_AI"
+              demoLink="https://plant49-ai.herokuapp.com/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={suicide}
+              isBlog={false}
+              title="Ai For Social Good"
+              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
+              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={emotion}
+              isBlog={false}
+              title="Face Recognition and Emotion Detection"
+              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
+              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
+              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+            />
+          </Col> */}
+        </Row>
+      </Container>
+      <h2 style={{color:"white"}}>New projects will be uploaded after constuct week & Full stack blog app not completed yet,<br/>will be uploaded next week</h2>
+    </Container>
+  );
+}
+
+export default Projects;
